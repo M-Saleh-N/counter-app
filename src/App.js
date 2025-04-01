7333cfc4c0cf027c278b7b153c6fa7dce1592783
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-01 15:40:01 +0300

    added content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React, {useState} from "react";
 function App() {
   return (
     <div className="cont">
-      
+      <div className="card">
+        <div className="card-body">
+          <h1> Counter App</h1>
+          <div className=""></div>
+        </div>
+
+      </div>
     </div>
   );
 }
