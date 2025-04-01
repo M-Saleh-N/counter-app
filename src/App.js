e5793892d370e50fd9ef9440b516d0344f5d02d6
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-01 16:32:15 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, {useState} from "react";
 
 
 function App() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const incrementButton = () => {
+    setCount(count + 1)
+  }
+  const decrementButton = () => {
+    setCount(count - 1)
+  }
+ 
   return (
     <div className="cont">
       <div className="card">
         <div className="card-body">
           <h1> Counter App</h1>
           <div className="s-cont">
-            <h2> COUNT</h2>
-            <button className="btn btn-primary"> increment</button>
-            <button className="btn btn-danger"> decrement</button>
+            <h2> {count}</h2>
+            <button onClick = {incrementButton}className="btn btn-primary"> increment</button>
+            <button onClick = {decrementButton}className="btn btn-danger"> decrement</button>
           </div>
         </div>
       </div>
